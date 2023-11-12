55d7c7940ecc0e4854d97a414f4edfa55add4874
wami ikechukwu <Wamiikechukwu@gmail.com>
2023-11-12 08:30:55 +0100

    fix: timestamp added in creation of users.

--- model/user.js
+++ model/user.js
@@ -1,7 +1,7 @@
 const db = require('../db')
 
 exports.create = async (data) => {
-    const result = await db.query(`INSERT INTO users (firstname, lastname, email, user_password, age, user_address, created_at, last_active, last_modified, account_locked, verified) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    const result = await db.query(`INSERT INTO users (firstname, lastname, email, user_password, age, user_address, created_at, last_active, last_modified, account_locked, verified) VALUES(?, ?, ?, ?, ?, ?, current_time(), current_time(),current_time() , ?, ?)`,
         [data.firstname, data.lastname, data.email, data.password, data.age, data.address, data.create_at, data.last_active, data.last_modified, data.account_locked, data.verified])
     return result;
 }
