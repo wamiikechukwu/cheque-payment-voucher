024402fd3458b40a570b283c96aa608f74d8d5d4
wami ikechukwu <Wamiikechukwu@gmail.com>
2023-11-27 07:57:58 +0100

    fix: added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,6 +12,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors())
+// for preflighting read more https://www.npmjs.com/package/cors
+app.options('*', cors())
 
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
